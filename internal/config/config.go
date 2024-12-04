44b719995fccbf380d5119e3183fcaa7f7ebc3d0
Evgeniy Romanov <nothing011235@gmail.com>
2024-12-04 17:12:09 +0500

    update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,7 +16,7 @@ type Config struct {
 func MustLoad() *Config {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file: %w", err)
 	}
 
 	var cfg Config
